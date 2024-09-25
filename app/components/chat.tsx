eef31e3adddc616641c95f7d36333438bb25439a
Eric <ericlauchiho@gmail.com>
2024-09-26 06:22:02 +0900

    update

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1526,7 +1526,7 @@ function _Chat() {
 
                   <div className={styles["chat-message-action-date"]}>
                     {isContext
-                      ? Locale.Chat.IsContext
+                      ? ""
                       : message.date.toLocaleString()}
                   </div>
                 </div>
