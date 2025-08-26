86c35a7e287246d3070a725b6224b2c123c9a2c0
Eric <ericlauchiho@gmail.com>
2025-08-26 09:45:01 +0800

    remove temperature param

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -61,7 +61,6 @@ export interface RequestPayload {
   }[];
   stream?: boolean;
   model: string;
-  temperature: number;
   presence_penalty: number;
   frequency_penalty: number;
   top_p: number;
@@ -229,7 +228,6 @@ export class ChatGPTApi implements LLMApi {
         messages,
         stream: options.config.stream,
         model: modelConfig.model,
-        temperature: !isO1OrO3 ? modelConfig.temperature : 1,
         presence_penalty: !isO1OrO3 ? modelConfig.presence_penalty : 0,
         frequency_penalty: !isO1OrO3 ? modelConfig.frequency_penalty : 0,
         top_p: !isO1OrO3 ? modelConfig.top_p : 1,
